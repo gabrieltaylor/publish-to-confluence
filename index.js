14b64da5bafcc6ceafd0994123089574a04ce745
Gabriel Taylor Russ <gabriel.taylor.russ@gmail.com>
2020-04-13 10:12:50 -0500

    Add convert markdown step

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 const core = require('@actions/core');
-const https = require('https');
+const fetch = require('node-fetch');
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
+async function convertToConfluenceMarkup(markdown) {
+    const data = JSON.stringify({wiki: markdown})
+
+    core.debug(data)
+
+    const url = buildUrl('/rest/tinymce/1/markdownxhtmlconverter')
+    const options = buildOptions(data)
+
+    core.debug(options)
+
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        core.debug(data)
+        return data;
+      })
+}
+
+async function publishToConfluence(markup) {
     let data = JSON.stringify({
       type: core.getInput('type'),
       title: core.getInput('title'),
       space: {key: core.getInput('space')},
       ancestors: [{id: core.getInput('parent_id')}],
       body: {
-        storage: {
-          value: core.getInput('body'),
-          representation: core.getInput('representation')
+        editor: {
+          value: markup,
+          representation: 'editor'
         }
       }
     })
 
     core.debug(data)
 
-    const options = {
-      hostname: core.getInput('hostname'),
-      port: 443,
-      path: '/wiki/rest/api/content/',
+    const url = buildUrl('/wiki/rest/api/content')
+    const options = buildOptions(data)
+
+    core.debug(options)
+
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        core.debug(data)
+        return data;
+      })
+}
+
+function buildOptions(data) {
+    return {
+      body: data,
       method: 'POST',
       headers: {
         'Authorization': 'Basic ' + new Buffer.from(core.getInput('username') + ':' + process.env.CONFLUENCE_API_TOKEN).toString('base64'),
@@ -30,23 +58,17 @@ async function run() {
         'Content-Length': data.length
       }
     }
+}
 
-    core.debug(options)
+function buildUrl(path) {
+    return `https://${core.getInput('hostname')}${path}`
+}
 
-    const req = https.request(options, res => {
-      core.debug(`statusCode: ${res.statusCode}`)
-
-      res.on('data', response_data => {
-        core.setOutput('response', response_data)
-      })
-    })
-
-    req.on('error', error => {
-      core.setFailed(error.message);
-    })
-
-    req.write(data)
-    req.end()
+async function run() {
+  try {
+    markup = await convertToConfluenceMarkup(core.getInput('body'));
+    reponse = await publishToConfluence(markup);
+    core.setOutput('response', response);
   }
   catch (error) {
     core.setFailed(error.message);
